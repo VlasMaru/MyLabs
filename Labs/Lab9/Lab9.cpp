581878a56d630f2aed9e0529de41005e0fbec32c
VlasMaru <118015689+VlasMaru@users.noreply.github.com>
2022-11-20 18:05:52 +0200

    Lab9
    +Lab9

---
+++ Labs/Lab9/Lab9.cpp
@@ -0,0 +1,59 @@
+﻿
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+
+class circle
+{
+private:
+	double m_pi = M_PI;
+	double m_rad = 0;
+	long double  m_area = 0;
+
+public:
+	circle(double rad)
+	{
+		m_rad = rad;
+		m_area = CircArea(rad);
+	}
+
+	~circle()
+	{
+		std::cout << "Destructor Wins!";
+	}
+
+	bool SetRad(double rad)
+	{
+		if (rad <= 0)
+		{
+			std::cout << "Rad Error!" << std::endl;
+			return false;
+		}
+		m_rad = rad;
+	}
+
+	double CircArea(double rad)
+	{
+		if (SetRad(rad))
+		{
+			m_area = (m_rad * m_rad) * m_pi;
+			return m_area;
+		}
+		return m_area;
+
+	}
+	void ShowArea()
+	{
+		if (m_area == 0) { std::cout << "The circle does not exist!" << std::endl; }
+		else { std::cout << m_area << std::endl; };
+	}
+};
+
+int main()
+{
+	double x;
+	std::cin >> x;
+	circle circ1(x);
+	circ1.ShowArea();
+}
+
