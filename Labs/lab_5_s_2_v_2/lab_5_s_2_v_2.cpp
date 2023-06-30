61f7e331663068a66feb3bb2277af7d1161c9314
VlasMaru <118015689+VlasMaru@users.noreply.github.com>
2023-07-01 01:14:46 +0200

    AAAARGH!

---
+++ Labs/lab_5_s_2_v_2/lab_5_s_2_v_2.cpp
@@ -0,0 +1,159 @@
+﻿#include<iostream>
+#include<vector>
+
+/*
+Реализуйте спецификацию шаблона для вектора из булевских значений. Помните, что на каждое значение должен быть потрачен только 1 бит памяти.
+Предусмотрите следующие операции :
+1.	Метод добавления в конец вектора
+2.	Метод и оператор получения и изменения значения по индексу
+3.	Метод size															+
+4.	Метод insert и erase
+
+Продемонстрируйте работу с классом.
+
+*/
+
+template <typename T>
+class vector
+{
+
+};
+
+template<>
+class vector<bool>
+{
+private:
+
+	size_t m_size = 0;
+	char* m_vec = new char[m_bytes];
+	size_t m_bytes = 0;
+
+public:
+
+	vector<bool>() = default;
+
+	vector(const vector<bool>& other)
+	{
+		vector<bool> tmp;
+		std::swap(tmp.m_vec, m_vec);
+		m_size = tmp.m_size;
+	}
+
+
+	vector<bool>& operator= (const vector<bool>& copy)
+	{
+		vector<bool> temp;
+		std::swap(temp.m_vec, m_vec);
+		m_size = temp.m_size;
+
+		return *this;
+	}
+
+	bool operator[](size_t ind) const
+	{
+		return static_cast<bool>(((m_vec[ind / 8]) >> ind) & 0b00000001);
+	}
+
+	void Reduct(size_t ind, bool el)
+	{
+		char digit = 1;
+
+		for (int i = 0; i < ind; i++)
+		{
+			digit *= 2;
+		}
+		if (el)
+		{
+			int byte_ind = ind / 8; // находим индекс байта в массиве
+			m_vec[byte_ind] |= digit; // устанавливаем бит, используя оператор побитового ИЛИ с маской digit
+		}
+		else
+		{
+			int byteIndex = ind / 8; // находим индекс байта в массиве
+			m_vec[byteIndex] &= ~digit; // сбрасываем бит, используя оператор побитового И с инвертированной маской ~digit
+		}
+	}
+
+	void Push_Back(bool el)
+	{
+		if (m_size % 8 == 0)
+		{
+			char* temp = new char[(m_size / 8) + 1];
+			for (int i = 0; i < m_size; i++)
+			{
+				temp[i] = m_vec[i];
+			}
+			delete[] m_vec;
+			m_vec = temp;
+			Reduct(m_size, el);
+		}
+		else
+		{
+			Reduct(m_size, el);
+		}
+
+		m_size++;
+	}
+
+	void Insert(size_t index, bool  value)
+	{
+		Push_Back(operator[](m_size - 1));
+		for (size_t i = m_size; i != index; i--)
+		{
+			Reduct(i, operator[](i - 1));
+		}
+
+		Reduct(index, value);
+	}
+
+	void Erase(size_t ind1, size_t ind2)
+	{
+		for (size_t i = ind2; i < m_size; i++)
+		{
+			Reduct(i - (ind2 - ind1), operator[](i));
+		}
+		m_size = m_size - (ind2 - ind1);
+	}
+
+	void Erase(size_t ind)
+	{
+		for (size_t i = ind; i < m_size - 1; i++)
+		{
+			Reduct(i, operator[](i + 1));
+		}
+		m_size--;
+	}
+
+	size_t Size()
+	{
+		return this->m_size;
+	}
+
+	void Print()
+	{
+		for (int i = 0; i < this->Size(); i++)
+		{
+			std::cout << this->operator[](i) << " ";
+		}
+	}
+
+	~vector<bool>()
+	{
+		delete[]m_vec;
+	}
+};
+
+int main()
+{
+	vector<bool> vc;
+	vc.Push_Back(1);
+	vc.Push_Back(1);
+	vc.Push_Back(0);
+	vc.Push_Back(1);
+	vc.Push_Back(1);
+
+	vc.Insert(4, 0);
+
+	vc.Print();
+
+}
---
+++ Labs/lab_8_s_2/lab_8_s_2.cpp
@@ -0,0 +1,114 @@
+﻿#include <iostream>
+
+/*
+Вашей задачей будет создать шаблонный класс для реализации стека. В нем нужно предусмотреть методы:
+1.	Конструктор с указанием размера стека                                                                      +++++
+2.	Метод size                                                                                         +++
+3.	Метод empty – возвращает true или false                                                                +++++
+4.	Метод push, чтобы положить в элемент в стек                                                                  ++++
+5.	Метод pop, чтобы извлечь элемент из стека                                                          +++++
+6.	Метод top, чтобы посмотреть верхний элемент стека, но не извлекать его.                                      +
+
+Добавьте обработку некорректных ситуаций через исключения:
+1.	Невозможно выделить необходимый участок памяти под стек (std::bad_alloc).                          +++
+2.	Невозможно добавить новый элемент в стек (например, std::overflow_error)                                     +
+3.	Невозможно извлечь элемент из стека, так как стек пустой (например, std::out_of_range)                       ++++
+4.	Невозможно посмотреть элемент на вершине стека, так как стек пустой (например, std::logic_error).        +
+
+*/
+
+template<typename T>
+class Stack
+{
+private:
+    size_t m_size = 0;
+    T* m_stack;
+    int m_top = 0;
+    bool m_empty = 1;
+
+public:
+    Stack(size_t size)
+    {
+        m_size = size;
+        m_stack = new T[size];
+    }
+    ~Stack()
+    {
+        delete[] m_stack;
+    }
+
+    size_t size()
+    {
+        return m_size;
+    }
+
+    bool get_empty()
+    {
+        return m_empty;
+    }
+
+    void push(const T& el)
+    {
+        if (m_top >= m_size)
+        {
+            throw std::out_of_range("[Full stack!]");
+        }
+        else
+        {
+            m_stack[m_top] = el;
+            m_top++;
+            m_empty = 0;
+        }
+    }
+
+    void pop()
+    {
+        if (m_empty)
+        {
+            throw std::out_of_range("[Empty stack!]");
+        }
+        else
+        {
+            m_top--;
+            if (m_top == 0)
+            {
+                m_empty = 1;
+            }
+        }
+    }
+
+    T top()
+    {
+        if (m_empty == 0)
+        {
+            return m_stack[m_top - 1];
+        }
+        else
+        {
+            throw std::logic_error("Top does not exist!");
+        }
+    }
+
+    
+};
+
+int main()
+{
+    try
+    {
+        Stack<int> s(2);
+        s.push(3);
+        s.push(6);
+        s.pop();
+        s.pop();
+        s.pop();
+    }
+    catch (const std::out_of_range& error)
+    {
+        std::cerr << "You cannot delete elements because of " << error.what()  << std::endl;
+    }
+    catch (const std::logic_error& error)
+    {
+        std::cerr << "You cannot refer to the top element because of " << error.what() << std::endl;
+    }
+}
